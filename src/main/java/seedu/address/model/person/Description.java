01584429979403a060de22633c28bd7a146b13f6
Jiwei <jiwei@Jiweis-MacBook-Pro-2.local>
2021-01-08 14:46:34 +0800

    Added Title and Description files

---
+++ src/main/java/seedu/address/model/person/Description.java
@@ -0,0 +1,57 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a Person's address in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ */
+public class Title {
+
+    public static final String MESSAGE_CONSTRAINTS = "Title can take any values, and it should not be blank";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[^\\s].*";
+
+    public final String title;
+
+    /**
+     * Constructs an {@code Address}.
+     *
+     * @param title A valid task title.
+     */
+    public Title(String title) {
+        requireNonNull(title);
+        checkArgument(isValidAddress(title), MESSAGE_CONSTRAINTS);
+        this.title = title;
+    }
+
+    /**
+     * Returns true if a given string is a valid email.
+     */
+    public static boolean isValidAddress(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Title // instanceof handles nulls
+                && title.equals(((Title) other).title)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return title.hashCode();
+    }
+
+}
